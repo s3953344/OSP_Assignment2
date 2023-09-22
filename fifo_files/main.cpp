52710bfba5a4b09e22f47d4ed4afdf8038d022d2
Frederick Hadi <s3953344@student.rmit.edu.com>
2023-09-22 11:50:30 +1000

    commented fifo calculations

--- fifo_files/main.cpp
+++ fifo_files/main.cpp
@@ -30,33 +30,33 @@ int main(int argc, char** argv) {
   return EXIT_SUCCESS;
 }
 
-/*
+/**
   * FIFO CALCULATIONS
+  * For FIFO
+  * Turnaround time = wait time + total time
+  * Response time = wait time
 */
 void fifo(std::deque<pcb*> rq) {
+  // first process has 0 wait time
   rq.front()->total_wait_time = 0;
   rq.front()->time_used = rq.front()->total_time;
 
+  // update time values for each process
   for (int i = 1; i < rq.size(); ++i) {
+    // each process waits for every process before it
     rq.at(i)->total_wait_time = rq.at(i - 1)->total_wait_time + rq.at(i - 1)->time_used;
+    // time used is the same as the burst time because the job must finish
     rq.at(i)->time_used = rq.at(i)->total_time;
-    // cout << rq.at(i)->total_wait_time << endl;
   }
 
-  
-  
-  // osp2023::time_type totalWait;
-  /**
-   * For FIFO
-   * Turnaround time = wait time + total time
-   * Response time = wait time
-  */
   osp2023::time_type totalWait = 0;
   osp2023::time_type totalTA = 0;
-  
+
   for (int i = 0; i < rq.size(); ++i) {
+    // update totals
     totalWait += rq.at(i)->total_wait_time;
     totalTA += rq.at(i)->total_wait_time + rq.at(i)->total_time;
+    // calculate and display individual stats for each process
     cout << "PID: " << rq.at(i)->id << endl;
     cout << "\twait time: " << rq.at(i)->total_wait_time << endl;
     cout << "\tresponse time: " << rq.at(i)->total_wait_time << endl;
@@ -64,9 +64,11 @@ void fifo(std::deque<pcb*> rq) {
     cout << "----------------------" << endl;
   }
 
+  // calculate totals
   osp2023::time_type avgWait = totalWait / (osp2023::time_type) rq.size();
   osp2023::time_type avgTA = totalTA / (osp2023::time_type) rq.size();
 
+  // display total stats
   cout << "Average waiting time: " << avgWait << endl;
   cout << "Average response time: " << avgWait << endl;
   cout << "Average turnaround time: " << avgTA << endl;
